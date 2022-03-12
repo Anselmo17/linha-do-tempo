15930d78bb89c25d82924be318655f10fbca0bd9
Anselmo <ansinftec17@gmail.com>
2022-03-12 13:36:18 -0300

    Implementando correção ao step de historico de acontecimentos

--- src/app/componentes/steps/mockSteps.ts
+++ src/app/componentes/steps/mockSteps.ts
@@ -1,9 +1,11 @@
 const steps: any = [
-  { ano: 2021, lado:'left', mensagem: 'Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.' },
-  { ano: 2020, lado:'right', mensagem: 'Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.' },
-  { ano: 2019, lado:'left', mensagem: 'Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.' },
-  { ano: 2018, lado:'right', mensagem: 'Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.' },
-  { ano: 2017, lado:'left', mensagem: 'Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.' }
+  { ano: 2021, lado: 'left', mensagem: 'Começando a se ocupar cargos de liderança e se mostrando solicito para ajudar as pessoas a evoluirem na carreira.' },
+  { ano: 2020, lado: 'right', mensagem: 'Ganhando muita experiencia , subindo de cargo e começando a ajudar outras pessoas a começarem suas carreiras.' },
+  { ano: 2019, lado: 'left', mensagem: 'Sendo promovida e mudando de cargo.' },
+  { ano: 2018, lado: 'right', mensagem: 'Começando a evoluir na carreira.' },
+  { ano: 2017, lado: 'left', mensagem: 'Entregando os primeiros projetos.' },
+  { ano: 2016, lado: 'right', mensagem: 'Ganhando experiencia em TI.' },
+  { ano: 2012, lado: 'left', mensagem: 'Inicio na area de tecnologia.' }
 ];
 
 export { steps };
